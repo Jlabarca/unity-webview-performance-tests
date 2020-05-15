ac543f348dff2348b426160ec43679e7b37b2e4e
Julio Labarca <jlabarca25@gmail.com>
2020-05-15 17:57:43 -0400

    Tests folders

---
+++ Assets/Test2_communication/CommunicationWebView.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using Extensions;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CommunicationWebView : MonoBehaviour
+{
+    public string url;
+    public WebViewObject webViewObject;
+    public RectTransform rectTransform;
+
+    private IEnumerator Start()
+    {
+        webViewObject.Init(
+            msg =>
+            {
+                Debug.Log($"CallFromJS[{msg}]");
+            },
+            err: (msg) =>
+            {
+                Debug.Log($"CallOnError[{msg}]");
+            },
+            started: (msg) =>
+            {
+                Debug.Log($"CallOnStarted[{msg}]");
+            },
+            ld: (msg) =>
+            {
+                Debug.Log($"CallOnLoaded[{msg}]");
+            },
+            //ua: "custom user agent string",
+            enableWKWebView: true);
+
+        webViewObject.SetRectTransformMargin(rectTransform);
+        //yield return webViewObject.LoadLocal(url);
+        Debug.Log("VAR");
+        var src = System.IO.Path.Combine(Application.streamingAssetsPath, url);
+        var dst = System.IO.Path.Combine(Application.persistentDataPath, url);
+        Debug.Log(src);
+        Debug.Log(dst);
+        byte[] result = null;
+        if (src.Contains("://")) {  // for Android
+            var www = UnityWebRequest.Get(src);
+            yield return www.SendWebRequest();
+            result = www.downloadHandler.data;
+        } else {
+            result = System.IO.File.ReadAllBytes(src);
+        }
+        System.IO.File.WriteAllBytes(dst, result);
+        webViewObject.LoadURL("file://" + dst.Replace(" ", "%20"));
+        webViewObject.SetVisibility(true);
+
+        yield break;
+    }
+
+    public void HideShowWebView()
+    {
+        var active = webViewObject.gameObject.activeSelf;
+        webViewObject.gameObject.SetActive(!active);
+        webViewObject.SetVisibility(!active);
+    }
+
+    public void GoBack()
+    {
+        webViewObject.GoBack();
+    }
+}
